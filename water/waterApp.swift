b229be30f2cd7a8ce1da81dcdd52611b6fd5edcc
Kody Deda <kodydeda4@gmail.com>
2023-02-14 14:36:19 -0500

    Initial Commit

---
+++ water/waterApp.swift
@@ -0,0 +1,17 @@
+//
+//  waterApp.swift
+//  water
+//
+//  Created by Kody Deda on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct waterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
